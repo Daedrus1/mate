c3cb530dfeac3178f5ab6d35284792b68189aa93
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-05-11 16:23:25 +0200

    first pom xml

---
+++ src/test/java/mate/academy/mate/MateApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
